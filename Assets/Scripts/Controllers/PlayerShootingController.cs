1ba1f823440ebc9aed2150d42aeaf889decb406f
Artur Perec <artur.g.perec@gmail.com>
2017-12-16 15:30:08 +0100

    Added new scene, Player and bullet placeholder, simple materials and pooling scripts

---
+++ Assets/Scripts/Controllers/PlayerShootingController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class PlayerShootingController : MonoBehaviour
+{
+
+    public float FireTime = 0.5f;
+    public GameObject Bullet;
+
+    public int pooledAmount = 20;
+    private List<GameObject> _bullets;
+    
+
+	void Start () {
+		_bullets = new List<GameObject>();
+	    for (int i = 0; i < pooledAmount; i++)
+	    {
+	        GameObject obj = (GameObject) Instantiate(Bullet);
+	        obj.SetActive(false);
+            _bullets.Add(obj);
+	    }
+
+        InvokeRepeating("Fire", FireTime, FireTime);
+	}
+	
+	void Fire ()
+	{
+	    for (int i = 0; i < _bullets.Count; i++)
+	    {
+	        if (!_bullets[i].activeInHierarchy)
+	        {
+	            _bullets[i].transform.position = transform.position;
+	            _bullets[i].transform.rotation = transform.rotation;
+	            _bullets[i].SetActive(true);
+	            break;
+	        }
+	    }
+	}
+}
---
+++ Assets/Scripts/Miscelanous/BulletDestroy.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class BulletDestroy : MonoBehaviour
+{
+
+    public float DestroyTime = 10f;
+
+    void OnEnable()
+    {
+        Invoke("Destroy", DestroyTime);
+    }
+
+    void Destroy()
+    {
+        gameObject.SetActive(false);
+    }
+
+    void OnDisable()
+    {
+        CancelInvoke();
+    }
+}
