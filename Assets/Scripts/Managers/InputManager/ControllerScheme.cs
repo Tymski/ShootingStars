53376d213700d0931595345e101933baf2b7a011
Mateusz Pusty <mateusz@veare.org>
2017-12-16 18:48:00 +0100

    Added custom input manager for 8 players for Xbox Gamepads

---
+++ Assets/Scripts/Managers/InputManager/ControllerScheme.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using GamepadInput;
+
+namespace Managers.InputManager
+{
+    [CreateAssetMenu(menuName = "Klagenfurt/Controller Scheme")]
+    public class ControllerScheme : ScriptableObject
+    {
+        public bool IsGamepad;
+        public bool IsRightPlayer;
+        public GamePad.Index ControllerIndex;
+
+        public string AxisX;
+        public string AxisY;
+        public KeyCode ButtonA;
+        public KeyCode ButtonB;
+        public KeyCode ButtonX;
+        public KeyCode ButtonY;
+    }
+}
