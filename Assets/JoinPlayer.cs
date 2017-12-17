ff913c0e925e2b683d718936ef39756fc2174b31
Artur Perec <artur.g.perec@gmail.com>
2017-12-17 01:41:01 +0100

    PlayerChose Menu WIP

---
+++ Assets/JoinPlayer.cs
@@ -0,0 +1,21 @@
+﻿using Managers.InputManager;
+using UnityEngine;
+
+public class JoinPlayer : MonoBehaviour {
+
+    private PlayerMovementController _playerMovementController;
+
+    private void Awake()
+    {
+        _playerMovementController = GetComponent<PlayerMovementController>();
+    }
+
+    void Update()
+    {
+        //        if (_playerMovementController.Player.GetButtonDown(PlayerButton.A))
+        if (Input.GetButtonDown("q"))
+        {
+            transform.GetChild(0).gameObject.SetActive(true);
+        }
+    }
+}
