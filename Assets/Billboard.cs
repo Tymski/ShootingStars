790e87da63988bd23dd25187caf1a16c2db3c57f
Mateusz Pusty <mateusz@veare.org>
2017-12-17 13:27:06 +0100

    Added Billboard script

---
+++ Assets/Billboard.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    public Camera m_Camera;
+
+    void Update()
+    {
+        transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward,
+            Camera.main.transform.rotation * Vector3.up);
+    }
+}
