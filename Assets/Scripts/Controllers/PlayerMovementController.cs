321ec7ad161a626f8e338e09325c2bde7c1e6681
Mateusz Pusty <mateusz@veare.org>
2017-12-17 09:23:43 +0100

    Re-added all assets

--- Assets/Scripts/Controllers/PlayerMovementController.cs
+++ Assets/Scripts/Controllers/PlayerMovementController.cs
@@ -6,18 +6,29 @@ using UnityEngine;
 public class PlayerMovementController : MonoBehaviour
 {
     [SerializeField]
-    private int _playerId;
+    public int PlayerId;
 
     [SerializeField]
     private float _speed;
 
-    private  CharacterController _characterController;
+    //private  CharacterController _characterController;
+
+    private Rigidbody _rigidbody;
 
     private Vector3 _moveDirection;
 
     private float _direction;
 
     private Player _player;
+
+    [SerializeField]
+    private SpriteRenderer _starRenderer;
+
+    [SerializeField]
+    private SpriteRenderer _playerRenderer;
+
+
+    private Animator _animator;
 
     public Vector3 MoveDirection
     {
@@ -29,24 +40,61 @@ public class PlayerMovementController : MonoBehaviour
         get { return _player; }
     }
 
+    private void UpdateAnimator()
+    {
+        _animator.SetBool("isWalking", _moveDirection.x != 0 || _moveDirection.z != 0);
+
+        _playerRenderer.flipX = false;
+        _starRenderer.flipX = false;
+
+        if (_moveDirection.z > 0)
+        {
+            _animator.SetBool("side", false);
+            _animator.SetBool("up", true);
+            _animator.SetBool("down", false);
+        }
+        else if (_moveDirection.z < 0)
+        {
+            _animator.SetBool("side", false);
+            _animator.SetBool("up", false);
+            _animator.SetBool("down", true);
+        }
+        else if (_moveDirection.x > 0 || _moveDirection.x < 0)
+        {
+            if (_moveDirection.x < 0)
+            {
+                _playerRenderer.flipX = true;
+                _starRenderer.flipX = true;
+            }
+
+            _animator.SetBool("side", true);
+             _animator.SetBool("up", false);
+             _animator.SetBool("down", false);
+        }
+    }
+
     void Awake()
     {
-        _characterController = GetComponent<CharacterController>();
+        _rigidbody = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
     }
 
     private void Start()
     {
-        _player = GameManager.Instance.InputManager.GetPlayer(_playerId);
+        _player = GameManager.Instance.InputManager.GetPlayer(PlayerId);
     }
 
     void Update()
     {
         _moveDirection = new Vector3(_player.GetAxis(PlayerAxis.HORIZONTAL), 0, _player.GetAxis(PlayerAxis.VERTICAL));
 
-        _moveDirection = new Vector3(_moveDirection.x / _moveDirection.magnitude, 0, _moveDirection.z / _moveDirection.magnitude);
-
-        _moveDirection *= _speed;
-
-        _characterController.Move(_moveDirection * Time.deltaTime);
+        UpdateAnimator();
     }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.position = new Vector3(_rigidbody.position.x + _moveDirection.x * _speed * Time.deltaTime,
+            _rigidbody.position.y, _rigidbody.position.z + _moveDirection.z * _speed * Time.deltaTime);
+    }
+
 }
